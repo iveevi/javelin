8e1345721445a50930719646b0cf29c2ef629a79
Venkataram Edavamadathil Sivaram <vesion4690@gmail.com>
2024-08-06 23:21:12 -0700

    Coarse roadmap

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
 # Javelin
 
 A realtime rendering framework.
+
+# Roadmap
+
+## Intermediate Representation Emitter (IRE)
+
+- [x] Revised implementation of vector types with limited swizzling
+- [x] Uniform layout concept for custom structs
+- [x] Layout input and output variables
+- [x] Push constants
+- [x] Persistent storage for mutating variables.
+- [x] Structure types.
+- [ ] Standard floating point functions (sin, cos, exp, log, etc.)
+- [ ] Standard vector operations (dot, cross, etc.)
+- [ ] Named structures for synthesizing compatible code
+- [ ] Uniform buffers
+- [ ] Simple SPIRV synthesis
+
+## Host-side Rendering
+
+- [x] Thread pool implementation
+- [ ] GGX material description
+- [ ] BVH construction methods
+- [ ] Naive path tracing
+- [ ] Path tracing with BRDF importance sampling
+- [ ] Path tracing with NEE
+- [ ] MIS path tracer
+
+## Testing
+
+- [ ] Local filecheck-like methods for comparing output
+- [ ] Generate tests for simple cases
--- source/core/preset.cpp
+++ source/core/preset.cpp
@@ -7,12 +7,10 @@
 #include "core/preset.hpp"
 
 template <>
-struct std::hash<tinyobj::index_t> {
-	size_t operator()(const tinyobj::index_t &k) const
-	{
-		auto h = hash<int>();
-		return ((h(k.vertex_index) ^ (h(k.normal_index) << 1)) >> 1) ^
-		       (h(k.texcoord_index) << 1);
+struct std::hash <tinyobj::index_t> {
+	size_t operator()(const tinyobj::index_t &k) const {
+		auto h = hash <int> ();
+		return ((h(k.vertex_index) ^ (h(k.normal_index) << 1)) >> 1) ^ (h(k.texcoord_index) << 1);
 	}
 };
 
@@ -20,15 +18,15 @@ namespace tinyobj {
 
 inline bool operator==(const tinyobj::index_t &a, const tinyobj::index_t &b)
 {
-	return std::tie(a.vertex_index, a.normal_index, a.texcoord_index) ==
-	       std::tie(b.vertex_index, b.normal_index, b.texcoord_index);
+	return std::tie(a.vertex_index, a.normal_index, a.texcoord_index)
+		== std::tie(b.vertex_index, b.normal_index, b.texcoord_index);
 }
 
 } // namespace tinyobj
 
 namespace jvl::core {
 
-std::optional<Preset> Preset::from(const std::filesystem::path &path)
+std::optional <Preset> Preset::from(const std::filesystem::path &path)
 {
 	Preset preset;
 	preset.path = path;
@@ -55,26 +53,25 @@ std::optional<Preset> Preset::from(const std::filesystem::path &path)
 	for (size_t s = 0; s < shapes.size(); s++) {
 		fmt::println("SHAPE>>>");
 
-		buffer<float3> positions;
-		buffer<float3> normals;
-		buffer<float2> uvs;
+		buffer <float3> positions;
+		buffer <float3> normals;
+		buffer <float2> uvs;
 
-		buffer<int3> triangles;
-		buffer<int4> quadrilaterals;
+		buffer <int3> triangles;
+		buffer <int4> quadrilaterals;
 
-		std::unordered_map<float3, int32_t> position_map;
-		std::unordered_map<tinyobj::index_t, int32_t> index_map;
+		std::unordered_map <float3, int32_t> position_map;
+		std::unordered_map <tinyobj::index_t, int32_t> index_map;
 
 		// tinyobj::shape_t;
 
 		// Loop over faces (polygon)
 		size_t index_offset = 0;
-		for (size_t f = 0; f < shapes[s].mesh.num_face_vertices.size();
-		     f++) {
+		for (size_t f = 0; f < shapes[s].mesh.num_face_vertices.size(); f++) {
 			size_t fv = size_t(shapes[s].mesh.num_face_vertices[f]);
 
 			// Triangles or quadrilaterals
-			int32_t indices[4]{-1, -1, -1, -1};
+			int32_t indices[4] { -1, -1, -1, -1 };
 
 			// Loop over vertices in the face.
 			for (size_t v = 0; v < fv; v++) {
@@ -87,23 +84,16 @@ std::optional<Preset> Preset::from(const std::filesystem::path &path)
 					continue;
 				}
 
-				tinyobj::real_t vx =
-					attrib.vertices[3 * idx.vertex_index +
-							0];
-				tinyobj::real_t vy =
-					attrib.vertices[3 * idx.vertex_index +
-							1];
-				tinyobj::real_t vz =
-					attrib.vertices[3 * idx.vertex_index +
-							2];
+				tinyobj::real_t vx = attrib.vertices[3 * idx.vertex_index + 0];
+                                tinyobj::real_t vy = attrib.vertices[3 * idx.vertex_index + 1];
+                                tinyobj::real_t vz = attrib.vertices[3 * idx.vertex_index + 2];
 
-				float3 p{vx, vy, vz};
+                                float3 p{vx, vy, vz};
 
 				if (position_map.count(p)) {
 					indices[v] = position_map[p];
 					continue;
-				}
-				else {
+				} else {
 					indices[v] = positions.size();
 				}
 
@@ -112,43 +102,26 @@ std::optional<Preset> Preset::from(const std::filesystem::path &path)
 				// Check if `normal_index` is zero or positive.
 				// negative = no normal data
 				if (idx.normal_index >= 0) {
-					tinyobj::real_t nx =
-						attrib.normals
-							[3 * size_t(idx.normal_index) +
-							 0];
-					tinyobj::real_t ny =
-						attrib.normals
-							[3 * size_t(idx.normal_index) +
-							 1];
-					tinyobj::real_t nz =
-						attrib.normals
-							[3 * size_t(idx.normal_index) +
-							 2];
+					tinyobj::real_t nx = attrib.normals[3 * size_t(idx.normal_index) + 0];
+					tinyobj::real_t ny = attrib.normals[3 * size_t(idx.normal_index) + 1];
+					tinyobj::real_t nz = attrib.normals[3 * size_t(idx.normal_index) + 2];
 					normals.push_back({nx, ny, nz});
 				}
 
 				// Check if `texcoord_index` is zero or
 				// positive. negative = no texcoord data
 				if (idx.texcoord_index >= 0) {
-					tinyobj::real_t tx =
-						attrib.texcoords
-							[2 * size_t(idx.texcoord_index) +
-							 0];
-					tinyobj::real_t ty =
-						attrib.texcoords
-							[2 * size_t(idx.texcoord_index) +
-							 1];
+					tinyobj::real_t tx = attrib.texcoords[2 * size_t(idx.texcoord_index) + 0];
+					tinyobj::real_t ty = attrib.texcoords[2 * size_t(idx.texcoord_index) + 1];
 					uvs.push_back({tx, ty});
 				}
 			}
 
 			if (indices[3] == -1) {
-				int3 f{indices[0], indices[1], indices[2]};
+				int3 f { indices[0], indices[1], indices[2] };
 				triangles.push_back(f);
-			}
-			else {
-				int4 f{indices[0], indices[1], indices[2],
-				       indices[3]};
+			} else {
+				int4 f { indices[0], indices[1], indices[2], indices[3]};
 				quadrilaterals.push_back(f);
 			}
 
@@ -174,8 +147,7 @@ std::optional<Preset> Preset::from(const std::filesystem::path &path)
 		if (triangles.size())
 			mesh.face_properties[Mesh::triangle_key] = triangles;
 		if (quadrilaterals.size())
-			mesh.face_properties[Mesh::quadrilateral_key] =
-				quadrilaterals;
+			mesh.face_properties[Mesh::quadrilateral_key] = quadrilaterals;
 
 		preset.geometry.push_back(mesh);
 	}
